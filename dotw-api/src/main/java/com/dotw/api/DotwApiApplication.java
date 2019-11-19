88fcf06185e290972677144d7bd0e3f2342bd9e6
yandong <20012002er@gmail.com>
2019-11-19 17:30:42 +0800

    inital

---
+++ dotw-api/src/main/java/com/dotw/api/DotwApiApplication.java
@@ -0,0 +1,18 @@
+package com.dotw.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lazyb
+ * @create 2019/11/19
+ * @desc
+ **/
+@SpringBootApplication
+public class DotwApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotwApiApplication.class, args);
+    }
+
+}
